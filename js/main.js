2c3485a94de57e9c86ab4447acaa7b843d6593e1
Redifa Islam <73694367+redifaislam-gif@users.noreply.github.com>
2021-04-27 14:45:36 +0600

    fff
    f

--- js/main.js
+++ js/main.js
@@ -1,43 +1,7 @@
-$(document).ready(function(){
+
 	
-	
-//stikey
-	$(".js--clint-section").waypoint(function(direction){
-		
-	if(direction== "down"){
-		$(".menu-section").addClass("stikey");
-	}else{
-	$(".menu-section").removeClass("stikey");	
-	}	
-		
-	});
-	//protfolio
-	var mixer=mixitup('.container');
-	
-	
-	$(".hellow-circle").circleProgress();	
-		
 	
 
-$(".hellow-circle").circleProgress();	
-	
-	
-	
-	
-	
-$('.owl-carosel').owlCarousel();	
-	
-});
-
-$(document).ready(function(){
-	
-	$(".logo").click(function(){
-		
-			$(this).slideIn();
-		
-	});	
-		
-});
 
 	
 
@@ -52,71 +16,7 @@ $(document).ready(function(){
 	  document.getElementById("myNav").style.width="0%";
   }
   
-
-
   
-  
-  
-  
-  
-  
-  
-  
-  
-  $(function(){
-	  
-	  let start =0;
-	  let end =$(".countup1").html();
-	  let speed =100;
-	  
-	  setInterval(function(){
-		  if(start<end){
-			  
-			  start++;
-		  }
-		  $(".countup1").html(start);
-		  
-	  }, speed);
-	  
-	  
-  });
-    
-  $(function(){
-	  
-	  let start =0;
-	  let end =$(".countup2").html();
-	  let speed =100;
-	  
-	  setInterval(function(){
-		  if(start<end){
-			  
-			  start++;
-		  }
-		  $(".countup2").html(start);
-		  
-	  }, speed);
-	  
-	  
-  });
-  
-    
-  $(function(){
-	  
-	  let start =0;
-	  let end =$(".countup3").html();
-	  let speed =100;
-	  
-	  setInterval(function(){
-		  if(start<end){
-			  
-			  start++;
-		  }
-		  $(".countup3").html(start);
-		  
-	  }, speed);
-	  
-	  
-  });
 
  
 
